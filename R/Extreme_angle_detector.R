aa5c6ac1d109754dedb190fbd13d5986c6679758
Perry <jp19193@essex.ac.uk>
2023-07-18 12:58:04 +0100

    updated from different file version

--- R/Extreme_angle_detector.R
+++ R/Extreme_angle_detector.R
@@ -1,14 +1,17 @@
-# simple bad edge detection detector
-Extreme_angle_detector <- function(dataset){
+Extreme_angle_detector <- function(data){
+
+  #cutting dataset into different portions based on content
+  dataset<- data %>% dplyr::select(starts_with("Angle_profile_"))
+
   # Iterate through the dataset row by row
   for (i in 1:nrow(dataset)) {
     # Check if any value in the row is over 280
     if (any(dataset[i, ] > 280)) {
       # Tag the row with 2 in "suspected detection error" column
-      dataset$suspected_detection_error[i] <- 2
+      data$suspected_detection_error[i] <- 2
     } else {
       # Tag the row with 1 in "suspected detection error" column
-      dataset$suspected_detection_error[i] <- 1
+      data$suspected_detection_error[i] <- 1
     }
   }
-  return(dataset)}
+  return(data)}
