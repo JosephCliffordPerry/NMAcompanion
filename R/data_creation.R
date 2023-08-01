272b47819c6ebc812eb4c25e65ee8ea0a00bc8b3
Perry <jp19193@essex.ac.uk>
2023-08-01 16:29:22 +0100

    added easier to use toy dataset

--- R/data_creation.R
+++ R/data_creation.R
@@ -1,17 +1,19 @@
-#' @name NMA_toy_dataset
-#' @title NMA toy dataset - A dataset with five different phenotypes
+#' NMA toy dataset - A dataset With various phenotypes
 #'
+#'@name NMA_toy_dataset
 #'
-#'@description This is a dataset made from a Phodophus Cambelli, a fbxo7 mutant
-#'and a PWK/lewes cross that has five different phenotypes in it. It is rounded
-#'to two decimals and sampled to save space
+#'@description This is a dataset made from a fbxo7 mutant that has at least two
+#' different phenotypes in it. It is rounded to two decimals and sampled to save
+#' space
 #'
-#'@format ## "NMA_toy_dataset" a data frame with all the variables exported in an NMA nuclear
-#'measurements file eg:Angle profiles, radius profiles, circularity
+#'@format ## "NMA_toy_dataset" a data frame with all the variables exported in
+#'an NMA nuclear measurements file eg:Angle profiles, radius profiles and
+#'circularity
 #'
 #'@source SkinnerLab NMA data
 #'
 
+data("NMA_toy_dataset", package = "NMAcompanion")
 
 
 
