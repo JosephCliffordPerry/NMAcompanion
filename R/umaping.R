a47f097186be894fb9bfccccc5f383b7aade9a22
Joe <jp19193@essex.ac.uk>
2023-08-03 15:29:42 +0100

    separated out umap functions for convinience

---
+++ R/umaping.R
@@ -0,0 +1,18 @@
+Umaping <- function(originaldata, angle_data, diameter_data, radius_data){
+
+numdata <- dplyr::select_if(originaldata, is.numeric)
+print("umap1 started")
+umapo <- umap(numdata, preserve.seed = TRUE)
+print("umap1 done")
+print("umap2 started")
+angleumap <- umap(angle_data, preserve.seed = TRUE)
+print("umap2 done")
+print("umap3 started")
+diameterumap <- umap(diameter_data, preserve.seed = TRUE)
+print("umap3 done")
+print("umap4 started")
+radiusumap <- umap(radius_data, preserve.seed = TRUE)
+print("umap4 done")
+umaplist<- list(umapo,angleumap,diameterumap,radiusumap)
+return(umaplist)
+}
