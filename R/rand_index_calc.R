8d6ed346f057e76a6f90a2d1774ef26afd701383
Joe <jp19193@essex.ac.uk>
2023-10-12 14:16:48 +0100

    added miniumaps to image output and made rand index Id calculation function

--- R/rand_index_calc.R
+++ R/rand_index_calc.R
@@ -1,25 +1,26 @@
 # #install.packages("fossil")
 # #library(fossil)
 # #rand index calc
-# make_randindex_data <- function(data,clusters) {
-#   clusties<-data$CellID
-#
-#   for (i in 1:length(clusters)) {
-#     x <- clusters[[i]][["Clustering_file"]]
-#     # Assign a unique name to x based on the cluster index (i)
-#     col_name <- paste0("Clustering_", i)
-#     # Create a new data frame with the same number of rows as data
-#     x_df <- data.frame(x = x)
-#
-#     # Rename the column to col_name
-#     colnames(x_df) <- col_name
-#     # Combine x into clusties with the assigned column name
-#     clusties <- cbind(clusties, x_df)
-#   }
-# return(clusties)}
-#
-# #rand_data<-make_randindex_data(data = data,clusters = clusters)
-# ###########
+#' @importFrom fossil rand.index
+make_randindex_data <- function(data,clusters) {
+  clusties<-data$CellID
+
+  for (i in 1:length(clusters)) {
+    x <- clusters[[i]][["Clustering_file"]]
+    # Assign a unique name to x based on the cluster index (i)
+    col_name <- paste0("Clustering_", i)
+    # Create a new data frame with the same number of rows as data
+    x_df <- data.frame(x = x)
+
+    # Rename the column to col_name
+    colnames(x_df) <- col_name
+    # Combine x into clusties with the assigned column name
+    clusties <- cbind(clusties, x_df)
+  }
+return(clusties)}
+
+#rand_data<-make_randindex_data(data = data,clusters = clusters)
+###########
 # sampled_rows <- sample(nrow(rand_data), size = 10000, replace = FALSE)
 #
 #
@@ -27,29 +28,29 @@
 #
 # sample_rand_data<rand_data[sample_rand_data,]
 # ######################
-# calculate_rand_indexes <- function(rand_data) {
-#   n <- ncol(rand_data)
-#
-#   result_matrix <- matrix(NA, nrow = n - 1, ncol = n - 1)
-#
-#   for (i in 2:(n-1)) {
-#     for (j in (i + 1):n) {
-#       # Extract the vectors from the data frame
-#       vector1 <- rand_data[[i]]
-#       vector2 <- rand_data[[j]]
-#
-#
-#
-#       rand_idx <- rand.index(vector1,vector2)
-#       result_matrix[i - 1, j - 1] <- rand_idx
-#       result_matrix[j - 1, i - 1] <- rand_idx
-#       # Print progress
-#       cat("Calculated Rand Index for", colnames(rand_data)[i], "vs", colnames(rand_data)[j], "\n")
-#     }
-#   }
-#
-#   return(result_matrix)
-# }
+calculate_rand_indexes <- function(rand_data) {
+  n <- ncol(rand_data)
+
+  result_matrix <- matrix(NA, nrow = n - 1, ncol = n - 1)
+
+  for (i in 2:(n-1)) {
+    for (j in (i + 1):n) {
+      # Extract the vectors from the data frame
+      vector1 <- rand_data[[i]]
+      vector2 <- rand_data[[j]]
+
+
+
+      rand_idx <- rand.index(vector1,vector2)
+      result_matrix[i - 1, j - 1] <- rand_idx
+     # result_matrix[j - 1, i - 1] <- rand_idx
+      # Print progress
+      cat("Calculated Rand Index for", colnames(rand_data)[i], "vs", colnames(rand_data)[j], "\n")
+    }
+  }
+
+  return(result_matrix)
+}
 #
 #
 # rand_matrix <- calculate_rand_indexes(sample_rand_data)
