0912b8c5afdc218ee42ead611652f268f142b190
Perry <jp19193@essex.ac.uk>
2025-05-08 14:20:52 +0100

    fixing documentation and fixing check functions

---
+++ R/zzz.R
@@ -0,0 +1,9 @@
+utils::globalVariables(c(
+  "Angle", "Cluster", "Cluster_characterising_ids", "Column1", "Column2",
+  "UUID", "UUIDs", "V1", "V2", "angle_data", "cluster$Clustering_file",
+  "clusters[[i]][[\"Clustering_file\"]]", "facet_count", "fill", "group",
+  "i", "median", "quantile", "setNames", "suspected_detection_error",
+  "title", "x", "y","kmeans"
+))
+#' @importFrom stats quantile median
+#' @importFrom utils read.table head setNames
