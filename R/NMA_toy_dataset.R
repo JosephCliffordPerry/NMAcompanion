26de17d6bcb5f8bf122c291760e419841aaed241
Perry <jp19193@essex.ac.uk>
2025-05-09 14:10:09 +0100

    added a simple import function

--- R/NMA_toy_dataset.R
+++ R/NMA_toy_dataset.R
@@ -3,9 +3,10 @@
 #' @title NMA_toy_dataset
 #' @name NMA_toy_dataset
 #'
-#' @description This is a dataset made from a fbxo7 mutant that has at least two
-#' different phenotypes in it. It is rounded to two decimals and sampled to save
-#' space
+#' @description This is a dataset made from the sperm  data of a
+#'  fbxo7 mutant mouse a phodopus sungorus hamster and a domestic pig.
+#'  It is rounded to two decimals and sampled to save
+#'  space
 #'
 #' @format ## "NMA_toy_dataset" a data frame with all the variables exported in
 #' an NMA nuclear measurements file eg:Angle profiles, radius profiles and
